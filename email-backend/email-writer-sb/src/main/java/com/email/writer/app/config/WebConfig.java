95ba5ce6011ecfba5db98c643d5eb00cb9210eb9
KAMAL MISHRA <139354803+Kamalmishra12@users.noreply.github.com>
2025-11-20 00:03:46 +0530

    Update WebConfig.java

--- email-backend/email-writer-sb/src/main/java/com/email/writer/app/config/WebConfig.java
+++ email-backend/email-writer-sb/src/main/java/com/email/writer/app/config/WebConfig.java
@@ -5,21 +5,19 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer  {
-
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                    https://email-generator-2-qs9c.onrender.com,
-                        "https://email-generator-murex.vercel.app"
+                        "https://email-generator-2-qs9c.onrender.com",
+                        "https://email-generator-murex.vercel.app",
+                        "https://email-generator2.onrender.com"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
     }
 }
-
