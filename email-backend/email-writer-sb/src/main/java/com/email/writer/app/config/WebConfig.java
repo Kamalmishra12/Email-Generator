09782557aa65305e3a9de78962a4f00fe8c9fe0d
KAMAL MISHRA <139354803+Kamalmishra12@users.noreply.github.com>
2025-11-19 22:04:44 +0530

    Add files via upload

---
+++ email-backend/email-writer-sb/src/main/java/com/email/writer/app/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.email.writer.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer  {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://email-generator-murex.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+}
